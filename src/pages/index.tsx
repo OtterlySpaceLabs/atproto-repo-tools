9b1a5f1e74a7a1aa26f0a5b9151adae7667b17cb
Alexandre Sanchez <contact@alexandresanchez.xyz>
2023-11-16 13:23:23 +0100

    feat: enhance form and response display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,65 @@
 import Head from "next/head"
-import { useCallback, useState } from "react"
+import { useTheme } from "next-themes"
+import { type FormEvent, useCallback, useMemo, useState } from "react"
+import { CodeBlock, atomOneDark, atomOneLight } from "react-code-blocks"
 
 import Header from "~/components/header/header"
 import { Button } from "~/components/shadcn/ui/button"
 import { Input } from "~/components/shadcn/ui/input"
 import { Label } from "~/components/shadcn/ui/label"
-import { Textarea } from "~/components/shadcn/ui/textarea"
 import { api } from "~/utils/api"
 
 export default function Home() {
 	const [handle, setHandle] = useState<string>("")
 	const [errorMsg, setErrorMsg] = useState<string | null>(null)
 	const [repo, setRepo] = useState<string | null>(null)
+	const [repoFetched, setRepoFetched] = useState<string | null>(null)
 
-	const archiveRepoMutation = api.repo.getRepoArchive.useMutation()
+	const theme = useTheme()
 
-	const handleArchiveRepo = useCallback(async () => {
-		if (!handle) return
-
-		try {
-			const result = await archiveRepoMutation.mutateAsync({ handle })
-			setRepo(JSON.stringify(result.contents, null, 2))
-		} catch (error) {
-			const errorVal = error as { message: string; statusCode: number }
-			if ("message" in errorVal && errorVal.message) {
-				setErrorMsg(errorVal.message)
-			}
+	const codeTheme = useMemo(() => {
+		if (theme.resolvedTheme === "dark" || theme.theme === "dark") {
+			return atomOneDark
+		} else {
+			return atomOneLight
 		}
-	}, [handle, archiveRepoMutation])
+	}, [theme])
+
+	const { mutateAsync, isLoading, isError, error } = api.repo.getRepoArchive.useMutation()
+
+	const handleArchiveRepo = useCallback(
+		async (e: FormEvent) => {
+			e.preventDefault()
+
+			if (!handle) {
+				setErrorMsg("Handle is required")
+				return
+			}
+			setErrorMsg(null)
+
+			if (repoFetched === handle) {
+				return
+			}
+
+			try {
+				const result = await mutateAsync({ handle })
+				setRepo(JSON.stringify(result.contents, null, 2))
+				setRepoFetched(handle)
+			} catch (err) {
+				const errorVal = err as { message: string; statusCode: number }
+				if ("message" in errorVal && errorVal.message) {
+					setErrorMsg(errorVal.message)
+				} else {
+					if (isError && error) {
+						setErrorMsg(error.message)
+					} else {
+						setErrorMsg("Something went wrong")
+					}
+				}
+			}
+		},
+		[handle, repoFetched, mutateAsync, isError, error]
+	)
 
 	return (
 		<>
@@ -38,25 +70,37 @@ export default function Home() {
 			<main className="min-h-screen bg-white dark:bg-black">
 				<Header />
 
-				<div className="mx-auto flex w-full max-w-3xl flex-col items-center justify-center gap-4">
-					<div>
-						<div className="grid w-full max-w-sm items-center gap-1.5">
-							<Label htmlFor="handle">Handle</Label>
+				<div className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-4">
+					<div className="flex flex-col gap-4">
+						<form method="post" onSubmit={handleArchiveRepo}>
+							<div className="grid w-full max-w-sm items-center gap-1.5">
+								<Label htmlFor="handle">Handle</Label>
 
-							<div className="flex w-full max-w-sm items-center space-x-2">
-								<Input
-									id="handle"
-									placeholder="alice.xyz"
-									onChange={(e) => setHandle(e.target.value)}
-								/>
-								<Button type="button" onClick={handleArchiveRepo}>
-									Get Archive
-								</Button>
+								<div className="flex w-full max-w-sm items-center space-x-2">
+									<Input
+										id="handle"
+										placeholder="alice.xyz"
+										onChange={(e) => setHandle(e.target.value)}
+										disabled={isLoading}
+									/>
+									<Button type="submit" disabled={isLoading}>
+										Get Archive
+									</Button>
+								</div>
 							</div>
-						</div>
+						</form>
+						{errorMsg && <div className="text-red-500 dark:text-red-400">{errorMsg}</div>}
 					</div>
-					<div className="w-full">
-						<Textarea value={repo ?? ""} rows={15} readOnly />
+					<div className="max-h-[75vh] w-full overflow-auto">
+						{repo && (
+							<CodeBlock
+								text={repo}
+								language="json"
+								theme={codeTheme}
+								showLineNumbers={false}
+								wrapLongLines
+							/>
+						)}
 					</div>
 				</div>
 			</main>
--- src/server/api/routers/repo/archive.ts
+++ src/server/api/routers/repo/archive.ts
@@ -5,35 +5,40 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
 import { getAtClient, resolveHandle } from "~/utils/atproto"
 
 export const repoArchiveRouter = createTRPCRouter({
-	getRepoArchive: publicProcedure.input(z.object({ handle: z.string() })).mutation(async ({ input }) => {
-		const { handle } = input
-		const resolved = await resolveHandle(handle)
+	getRepoArchive: publicProcedure
+		.input(z.object({ handle: z.string().transform((input) => (input.startsWith("@") ? input.slice(1) : input)) }))
+		.mutation(async ({ input }) => {
+			const { handle } = input
+			const resolved = await resolveHandle(handle)
 
-		if (!resolved) {
-			throw new Error(`Could not resolve handle ${handle}`)
-		}
+			if (!resolved) {
+				throw new Error(`Could not resolve handle ${handle}`)
+			}
 
-		const { did, pds, signingKey } = resolved
+			const { did, pds, signingKey } = resolved
 
-		const client = getAtClient(pds)
+			const client = getAtClient(pds)
 
-		const repoPayload = await client.com.atproto.sync.getRepo({ did })
+			const repoPayload = await client.com.atproto.sync.getRepo({ did })
 
-		if (!repoPayload.success) {
-			throw new Error(`Could not get repo for ${handle} (${did})`)
-		}
+			if (!repoPayload.success) {
+				throw new Error(`Could not get repo for ${handle} (${did})`)
+			}
 
-		const repoData = repoPayload.data
+			const repoData = repoPayload.data
 
-		const car = await readCarWithRoot(repoData)
+			const car = await readCarWithRoot(repoData)
 
-		const verified = await verifyRepo(car.blocks, car.root, did, signingKey)
+			const verified = await verifyRepo(car.blocks, car.root, did, signingKey)
 
-		const syncStorage = new MemoryBlockstore()
-		await syncStorage.applyCommit(verified.commit)
-		const loadedRepo = await Repo.load(syncStorage, car.root)
-		const contents = await loadedRepo.getContents()
+			const syncStorage = new MemoryBlockstore()
+			await syncStorage.applyCommit(verified.commit)
+			const loadedRepo = await Repo.load(syncStorage, car.root)
+			const contents = await loadedRepo.getContents()
 
-		return { repoData, contents }
-	})
+			return {
+				// repoData,
+				contents
+			}
+		})
 })
